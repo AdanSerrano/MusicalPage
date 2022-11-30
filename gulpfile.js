982cccc23ac05afa9e0b32ce570ba68ec5cd571f
Adan Serrano <adan.serrano@utp.ac.pa>
2022-11-30 14:05:00 -0500

    configuracion gulp cambiar formato de jpg a webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
+
+//css
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require ('gulp-plumber')
+
+
+// imagenes
+const webp = require('gulp-webp');
+
+
 
 function css(done) {
   // identificar el archivo
@@ -14,6 +22,22 @@ function css(done) {
   done();
 }
 
+
+// cambiar imagen de jpg png a webp
+function versionWebp(done) {
+
+  const opciones = {
+    quality: 50
+  };
+
+  src('src/img/**/*.{png, jpg}')
+    .pipe(webp(opciones))
+    .pipe(dest('build/img'))
+  
+  
+  done()
+}
+
 function dev(done) {
     watch('src/scss/**/*.scss', css);
 
@@ -22,4 +46,7 @@ function dev(done) {
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(versionWebp, dev);
+
+
